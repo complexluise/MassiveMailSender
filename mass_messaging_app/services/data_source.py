56b0ede0825461df39c2afba23d8cf08df78b885
complexluise <lhiguerac@unal.edu.co>
2024-04-24 09:03:18 -0500

    get_credentials and fetch contacts from gsheets

--- mass_messaging_app/services/data_source.py
+++ mass_messaging_app/services/data_source.py
@@ -2,10 +2,14 @@ from csv import DictReader
 
 from mass_messaging_app.models.models import Contact
 from mass_messaging_app.config.settings import GoogleServiceSettings
-from mass_messaging_app.services.google_service import fetch_contacts_from_sheet
+from mass_messaging_app.services.google_service import _fetch_contacts_from_sheet, _get_credentials_from_oauth
 
 
-def fetch_contacts(
+def get_google_credentials():
+    _get_credentials_from_oauth()
+
+
+def fetch_contacts_from_gsheets(
     source_type: str,
     source_settings: GoogleServiceSettings,
     spreadsheet_id: str,
