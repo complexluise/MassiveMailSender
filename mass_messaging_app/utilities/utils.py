889b934d6a584652e48a96f202580a2f9ee45b54
complexluise <lhiguerac@unal.edu.co>
2024-04-24 17:56:19 -0500

    remove comments and add body file

--- mass_messaging_app/utilities/utils.py
+++ mass_messaging_app/utilities/utils.py
@@ -22,14 +22,11 @@ def load_message_template(json_file_path: str) -> MessageTemplate:
     Returns:
     MessageTemplate: A MessageTemplate instance with subject and body loaded from specified files.
     """
-    # Get the directory of the JSON file to ensure relative paths are handled correctly
     directory = os.path.dirname(json_file_path)
 
-    # Load the JSON configuration
     with open(json_file_path, "r", encoding="utf-8") as file:
         template_data = json.load(file)
 
-    # Read the HTML content for the body from the specified file path
     body_file_name = template_data.get("body_file")
     if body_file_name:
         body_file_path = os.path.join(directory, body_file_name)
@@ -37,11 +34,28 @@ def load_message_template(json_file_path: str) -> MessageTemplate:
             body_content = file.read()
         template_data["body"] = body_content
     else:
-        # Default to an empty body if no file is specified
         template_data["body"] = ""
 
-    # Remove the 'body_file' key as it's no longer needed and not expected by the MessageTemplate model
+    body_file_name = template_data.get("body_file")
+    if body_file_name:
+        body_file_path = os.path.join(directory, body_file_name)
+        with open(body_file_path, "r", encoding="utf-8") as file:
+            body_content = file.read()
+        template_data["body"] = body_content
+    else:
+        template_data["body"] = ""
+
     template_data.pop("body_file", None)
 
-    # Create and return the MessageTemplate instance
+    attachment_file = template_data.get("attachment_file")
+    if attachment_file:
+        attachment_file = os.path.join(directory, attachment_file)
+        with open(attachment_file, "r", encoding="utf-8") as file:
+            attachment = file.read()
+        template_data["attachment"] = attachment
+    else:
+        template_data["attachment"] = ""
+
+    template_data.pop("attachment_file", None)
+
     return MessageTemplate(**template_data)
