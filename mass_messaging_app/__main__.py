c5ad9e17b1ea2a5325f2d6d7188805e243647ca3
complexluise <lhiguerac@unal.edu.co>
2026-04-06 09:01:33 -0500

    Rename file and change step in send_mail

--- mass_messaging_app/__main__.py
+++ mass_messaging_app/__main__.py
@@ -70,9 +70,10 @@ def main():
         contacts: list[Contact] = []
         if args.contacts_file:
             contacts: list[Contact] = fetch_contacts_from_csv(args.contacts_file)
+            send_mail(contacts, args.campaign)
         else:
             parser.error("Must specify either --contacts_file")
-        send_mail(contacts, args.campaign)
+        
 
 
 if __name__ == "__main__":
